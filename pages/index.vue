073d214855f01a96f80f88848abfa76509688be3
crypt0box <pray.happy76@gmail.com>
2020-05-24 11:01:43 +0900

    UPDATE: get user info

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
 <div>
   home
+  <p>userUid: {{ $store.getters.getUserUid }}</p>
+  <p>userEmail: {{ $store.getters.getUserEmail }}</p>
 </div>
 </template>
--- store/index.js
+++ store/index.js
@@ -1,5 +1,19 @@
 import firebase from '~/plugins/firebase'
 
+export const state = () => ({
+  userUid: '',
+  userEmail: '',
+})
+
+export const mutations = {
+  setUserUid(state, userUid) {
+    state.userUid = userUid
+  },
+  setUserEmail(state, userEmail) {
+    state.userEmail = userEmail
+  }
+}
+
 export const actions = {
   register({ dispatch }, authData) {
     firebase.auth().createUserWithEmailAndPassword(
@@ -11,14 +25,28 @@ export const actions = {
       this.$router.push('/')
     })
   },
-  login({ dispatch }, authData) {
+  login({ commit }, authData) {
     firebase.auth().signInWithEmailAndPassword(
       authData.email,
       authData.password,
     )
-    .then(response => {
-      console.log(response)
-      this.$router.push('/')
-    })
+    .then(function (result) {
+      const user = result.user;
+      console.log('success :' + user)
+      console.log('success :' + user.uid + ' : ' + user.email)
+      commit('setUserUid', user.uid)
+      commit('setUserEmail', user.email)
+    },
+    this.$router.push('/')
+    )
   },
+}
+
+export const getters = {
+  getUserUid(state) {
+    return state.userUid
+  },
+  getUserEmail(state) {
+    return state.userEmail
+  }
 }
